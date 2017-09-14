a5fe5629fb40a0f81f32fe13aa72591844fb4043
Vierkantor <vierkantor@vierkantor.com>
2017-09-14 20:39:40 +0000

    Support for block comments

--- Util.py
+++ Util.py
@@ -1,5 +1,8 @@
 # Util.py - Defines some handy tools
 
+class ParseError(Exception):
+	pass
+
 # adds two spaces in front of every line
 def Indent(string):
 	return "  " + "\n  ".join(string.splitlines());
@@ -12,6 +15,20 @@ def SkipWhitespace(string):
 		elif string[0:2] == "//":
 			while len(string) > 0 and string[0] != "\n":
 				string = string[1:];
+		elif string[0:2] == "/*":
+			string = string[2:]
+			depth = 1
+			while depth > 0:
+				if len(string) <= 0:
+					raise ParseError("unclosed comment")
+				if string[0:2] == "*/":
+					depth -= 1
+					string = string[2:]
+				elif string[0:2] == "/*":
+					depth += 1
+					string = string[2:]
+				else:
+					string = string[1:];
 		else:
 			break;
 	return string;
